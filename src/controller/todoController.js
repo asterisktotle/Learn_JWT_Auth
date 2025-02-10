66a6e50c61728d8fb990ad28725897c71b4541cd
asterisktotle <barandajander@gmail.com>
2025-02-10 23:30:34 +0800

    complete todo crud function in router

--- src/controller/todoController.js
+++ src/controller/todoController.js
@@ -7,7 +7,42 @@ export const getAllTodos = (req, res) => {
 	res.json(todos);
 };
 
+export const createTodo = (req, res) => {
+	const { task } = req.body;
+
+	//provide the columns with params then provide the values in the db
+	const insertTodo = db.prepare(
+		`INSERT INTO todos (user_id, task) VALUES (?, ?)`
+	);
+
+	insertTodo.run(req.userId, task);
+
+	res.json({ id: insertTodo.lastID, task, completed: 0 });
+};
+
+export const updateTodo = (req, res) => {
+	const { completed } = req.body;
+	const { id } = req.params;
+	const { page } = req.query;
+
+	const updatedTodos = db.prepare(
+		`UPDATE todos SET completed = ? WHERE id = ?`
+	);
+	updatedTodos.run(completed, id);
+
+	res.json({ message: 'Todo completed' });
+};
+
+export const deleteTodo = (req, res) => {
+	const { id } = req.params;
+	const { userId } = req;
+
+	const deleteTodos = db.prepare(
+		`DELETE FROM todos WHERE id = ? AND user_id = ?`
+	);
+	deleteTodos.run(id, userId);
+
+	res.send({ message: 'task deleted' });
+};
+
 export const getTodo = (req, res) => {};
-export const createTodo = (req, res) => {};
-export const updateTodo = (req, res) => {};
-export const deleteTodo = (req, res) => {};
