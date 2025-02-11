092fd7a74f34156976fff318964d2f5a6b4e40be
asterisktotle <barandajander@gmail.com>
2025-02-11 23:16:13 +0800

    set up prisma schema

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,8 +1,9 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import db from '../db.js';
+import prisma from '../prismaClient.js';
 
-export const createAccount = (req, res) => {
+export const createAccount = async (req, res) => {
 	console.log('Received body: ', req.body); //debug
 	const { username, password } = req.body;
 
@@ -12,23 +13,47 @@ export const createAccount = (req, res) => {
 
 	//error handling
 	try {
+		//method 1 - using sqlite
 		//prepare a  SQLite command and save the username and encrypted password
-		const insertUser = db.prepare(
-			`INSERT INTO users (username, password) VALUES (?, ?)`
-		);
-		const result = insertUser.run(username, hashedPassword);
+		// const insertUser = db.prepare(
+		// 	`INSERT INTO users (username, password) VALUES (?, ?)`
+		// );
+		// const result = insertUser.run(username, hashedPassword);
+
+		//method 2 - using prisma
+		const user = await prisma.user.create({
+			data: {
+				username,
+				password: hashedPassword,
+			},
+		});
 
 		//for the new user, add their first todo to the database
 		const defaultTodo = 'Hello! Add your first todo';
-		const insertTodo = db.prepare(
-			`INSERT INTO todos (user_id, task) VALUES (?,?)`
-		);
-		insertTodo.run(result.lastInsertRowid, defaultTodo);
+
+		//method 1 - sql
+		// const insertTodo = db.prepare(
+		// 	`INSERT INTO todos (user_id, task) VALUES (?,?)`
+		// );
+		// insertTodo.run(result.lastInsertRowid, defaultTodo);
+
+		// method 2 - prisma
+		await prisma.todo.create({
+			data: {
+				task: defaultTodo,
+				userId: user.id,
+			},
+		});
 
 		//create a token object that expires using jwt
 		//it includes rowId, secret token, expiry date
 		const token = jwt.sign(
-			{ id: result.lastInsertRowid },
+			//method 1 - sql
+			// { id: result.lastInsertRowid },
+
+			//method 2 - prisma
+			{ id: user.id },
+
 			process.env.JWT_SECRET,
 			{ expiresIn: '24h' }
 		);
@@ -42,13 +67,20 @@ export const createAccount = (req, res) => {
 	res.sendStatus(200);
 };
 
-export const loginAccount = (req, res) => {
+export const loginAccount = async (req, res) => {
 	const { username, password } = req.body;
 
 	try {
 		//find the  user from the database using SQL command
-		const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`);
-		const user = getUser.get(username);
+		// const getUser = db.prepare(`SELECT * FROM users WHERE username = ?`);
+		// const user = getUser.get(username);
+
+		//method 2
+		const user = await prisma.user.findUnique({
+			where: {
+				username: username,
+			},
+		});
 
 		//if user is not found
 		if (!user) {
--- src/controller/todoController.js
+++ src/controller/todoController.js
@@ -1,46 +1,78 @@
-import express from 'express';
 import db from '../db.js';
+import prisma from '../prismaClient.js';
 
-export const getAllTodos = (req, res) => {
-	const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?');
-	const todos = getTodos.all(req.userId);
+export const getAllTodos = async (req, res) => {
+	// const getTodos = db.prepare('SELECT * FROM todos WHERE user_id = ?');
+	const todos = await prisma.todo.findMany({
+		where: {
+			userId: req.userId,
+		},
+	});
+
+	// const todos = getTodos.all(req.userId);
 	res.json(todos);
 };
 
-export const createTodo = (req, res) => {
+export const createTodo = async (req, res) => {
 	const { task } = req.body;
 
 	//provide the columns with params then provide the values in the db
-	const insertTodo = db.prepare(
-		`INSERT INTO todos (user_id, task) VALUES (?, ?)`
-	);
+	// const insertTodo = db.prepare(
+	// 	`INSERT INTO todos (user_id, task) VALUES (?, ?)`
+	// );
 
-	insertTodo.run(req.userId, task);
+	// insertTodo.run(req.userId, task);
 
-	res.json({ id: insertTodo.lastID, task, completed: 0 });
+	const todo = await prisma.todo.create({
+		data: {
+			task,
+			userId: req.userId,
+		},
+	});
+
+	// res.json({ id: insertTodo.lastID, task, completed: 0 });
+	res.json(todo);
 };
 
-export const updateTodo = (req, res) => {
+export const updateTodo = async (req, res) => {
 	const { completed } = req.body;
 	const { id } = req.params;
-	const { page } = req.query;
+	// const { page } = req.query;
 
-	const updatedTodos = db.prepare(
-		`UPDATE todos SET completed = ? WHERE id = ?`
-	);
-	updatedTodos.run(completed, id);
+	// const updatedTodos = db.prepare(
+	// 	`UPDATE todos SET completed = ? WHERE id = ?`
+	// );
+	// updatedTodos.run(completed, id);
 
-	res.json({ message: 'Todo completed' });
+	const updatedTodo = await prisma.todo.update({
+		where: {
+			id: parseInt(id),
+			userId: req.userId,
+		},
+		data: {
+			completed: !!completed,
+		},
+	});
+
+	// res.json({ message: 'Todo completed' });
+	res.json(updatedTodo);
 };
 
-export const deleteTodo = (req, res) => {
+export const deleteTodo = async (req, res) => {
 	const { id } = req.params;
 	const { userId } = req;
 
-	const deleteTodos = db.prepare(
-		`DELETE FROM todos WHERE id = ? AND user_id = ?`
-	);
-	deleteTodos.run(id, userId);
+	// const deleteTodos = db.prepare(
+	// 	`DELETE FROM todos WHERE id = ? AND user_id = ?`
+	// );
+	// deleteTodos.run(id, userId);
+
+	const deleteTodos = await prisma.todo.delete({
+		where: {
+			id: parseInt(id),
+			userId,
+		},
+	});
 
 	res.send({ message: 'task deleted' });
 };
